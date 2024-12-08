4df2f9da1d3bd99787e1904ef273a3a0d8ef8440
ananya-dhiman <ananya.dhiman@somaiya.edu>
2024-12-08 11:00:34 +0530

    Basic function

--- app.js
+++ app.js
@@ -1,2 +1,19 @@
 const express=require('express');
 const app=express();
+
+const path = require("node:path");
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
+
+const assetsPath = path.join(__dirname, "public");
+app.use(express.static(assetsPath));
+
+app.use(express.urlencoded({ extended: true }));
+
+const mainRouter=require("./routes/index");
+app.use(mainRouter);
+
+const PORT=8080;
+app.listen(PORT,()=>{
+    console.log('Express app at 8080');
+})
--- routes/index.js
+++ routes/index.js
@@ -10,3 +10,19 @@ const messages = [
       added: new Date()
     }
 ];
+
+const {Router}=require("express");
+const mainRouter=Router();
+
+//Main page 
+mainRouter.get("/",(req,res)=>res.render("index",{messages:messages}));
+//Add note
+mainRouter.get("/new",(req,res,next)=>res.render("form"));
+mainRouter.post("/new",(req,res)=>{
+  messageText=req.body.messageText;
+  nameText=req.body.nameText;
+  console.log(req.body);
+  messages.push({text:messageText,user:nameText,added:new Date()});
+  res.redirect("/");
+});
+module.exports=mainRouter;
